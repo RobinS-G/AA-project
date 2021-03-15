129891c156dcd0c28bc3c99ef698dbc88597ae3d
Robin <robinsgpro@gmail.com>
2021-03-15 09:39:24 +0100

    New parameter (rotation)

--- src/sketch.ts
+++ src/sketch.ts
@@ -6,12 +6,12 @@ const gui = new dat.GUI()
 const params = {
     Nombre: 100,
     Random_Seed: 0,
-    P: 0.7,
+    Rotation: 0,
     Download_Image: () => save(),
 }
 gui.add(params, "Nombre", 0, 200, 1)
 gui.add(params, "Random_Seed", 0, 100, 1)
-gui.add(params, "P", 0, 1, 0.001)
+gui.add(params, "Rotation", 0, 180, 1)
 gui.add(params, "Download_Image")
 
 // -------------------
@@ -24,14 +24,18 @@ function draw() {
     background("white")
     translate(width/2, height/2)
     fill(255, 255, 255, 0)
+    rotate(params.Rotation)
     for (let angle = 0; angle < TWO_PI; angle += TWO_PI / params.Nombre) {
         let k=random(10,240)
         push()
             rotate(angle)
-            if(k<50){
-            rect(k, 0, random(1,25), random(1,5))
+            if(k<40){
+            rect(k, 0, random(1,25), random(1,3))
             }
-            else rect(k, 0, random(15,50), random(5,20))
+            else if(k<100){
+                rect(k, 0, random(7,30), random(3,10))
+            }
+            else rect(k, 0, random(15,70), random(5,20))
         pop()
     }
 }      

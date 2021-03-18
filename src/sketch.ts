9bb115f0fa4d692855a423d77260d6f5b1c76f4f
Robin <robinsgpro@gmail.com>
2021-03-18 21:09:29 +0100

    Ptite modif

--- src/sketch.ts
+++ src/sketch.ts
@@ -4,10 +4,10 @@
 
 const gui = new dat.GUI()
 const params = {
-    Nombre: 100,
+    Number: 100,
     Random_Seed: 0,
     Rotation: 0,
-    Uni_Dégradé: 0,
+    Plain_Gradient: 0,
     Red1:255,
     Green1: 255,
     Blue1:255,
@@ -19,10 +19,10 @@ const params = {
 
     Download_Image: () => save(),
 }
-gui.add(params, "Nombre", 0, 200, 1)
+gui.add(params, "Number", 0, 200, 1)
 gui.add(params, "Random_Seed", 0, 100, 1)
 gui.add(params, "Rotation", 0, 180, 1)
-gui.add(params, "Uni_Dégradé", 0, 1, 1)
+gui.add(params, "Plain_Gradient", 0, 1, 1)
 gui.add(params, "Red1", 0, 255, 1)
 gui.add(params, "Green1", 0, 255, 1)
 gui.add(params, "Blue1", 0, 255, 1)
@@ -42,18 +42,18 @@ function draw() {
     rectMode(CENTER)
     background("white")
     translate(width/2, height/2)
-    if(params.Uni_Dégradé==0){
+    if(params.Plain_Gradient==0){
         fill(params.Red1, params.Green1, params.Blue1, params.Transparency1)
     }
     let t=0
     rotate(params.Rotation)
-    for (let angle = 0; angle < TWO_PI; angle += TWO_PI / params.Nombre) {
+    for (let angle = 0; angle < TWO_PI; angle += TWO_PI / params.Number) {
         let k=random(10,240)
-        if (params.Uni_Dégradé==1){
+        if (params.Plain_Gradient==1){
             let color1=color(params.Red1, params.Green1, params.Blue1, params.Transparency1)
             let color2=color(params.Red2, params.Green2, params.Blue2, params.Transparency2)
             fill(lerpColor(color1,color2,t))
-            t+=1/params.Nombre
+            t+=1/params.Number
         }
         push()
             rotate(angle)

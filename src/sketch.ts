3357ff926bbf904bb9f3944aedf963f170445564
Robin <robinsgpro@gmail.com>
2021-03-18 20:03:19 +0100

    New parameter
    Gradient

--- src/sketch.ts
+++ src/sketch.ts
@@ -4,22 +4,33 @@
 
 const gui = new dat.GUI()
 const params = {
-    Nombre: 145,
+    Nombre: 100,
     Random_Seed: 0,
     Rotation: 0,
-    Red:255,
-    Green: 255,
-    Blue:255,
-    Transparency: 0,
+    Uni_Dégradé: 0,
+    Red1:255,
+    Green1: 255,
+    Blue1:255,
+    Transparency1: 0,
+    Red2:255,
+    Green2: 255,
+    Blue2:255,
+    Transparency2: 0,
+
     Download_Image: () => save(),
 }
 gui.add(params, "Nombre", 0, 200, 1)
 gui.add(params, "Random_Seed", 0, 100, 1)
 gui.add(params, "Rotation", 0, 180, 1)
-gui.add(params, "Red", 0, 255, 1)
-gui.add(params, "Green", 0, 255, 1)
-gui.add(params, "Blue", 0, 255, 1)
-gui.add(params, "Transparency", 0, 255, 1)
+gui.add(params, "Uni_Dégradé", 0, 1, 1)
+gui.add(params, "Red1", 0, 255, 1)
+gui.add(params, "Green1", 0, 255, 1)
+gui.add(params, "Blue1", 0, 255, 1)
+gui.add(params, "Transparency1", 0, 255, 1)
+gui.add(params, "Red2", 0, 255, 1)
+gui.add(params, "Green2", 0, 255, 1)
+gui.add(params, "Blue2", 0, 255, 1)
+gui.add(params, "Transparency2", 0, 255, 1)
 gui.add(params, "Download_Image")
 
 // -------------------
@@ -31,11 +42,19 @@ function draw() {
     rectMode(CENTER)
     background("white")
     translate(width/2, height/2)
-
-    fill(params.Red, params.Green, params.Blue, params.Transparency)
+    if(params.Uni_Dégradé==0){
+        fill(params.Red1, params.Green1, params.Blue1, params.Transparency1)
+    }
+    let t=0
     rotate(params.Rotation)
     for (let angle = 0; angle < TWO_PI; angle += TWO_PI / params.Nombre) {
         let k=random(10,240)
+        if (params.Uni_Dégradé==1){
+            let color1=color(params.Red1, params.Green1, params.Blue1, params.Transparency1)
+            let color2=color(params.Red2, params.Green2, params.Blue2, params.Transparency2)
+            fill(lerpColor(color1,color2,t))
+            t+=1/params.Nombre
+        }
         push()
             rotate(angle)
             if(k<40){
@@ -47,8 +66,7 @@ function draw() {
             else rect(k, 0, random(15,70), random(5,20))
         pop()
     }
-}       
-
+}   
 
 // -------------------
 //    Initialization

5120a6ab789f1f48477d44d2d605ddfa23ee5d1e
Robin <robinsgpro@gmail.com>
2021-03-19 20:56:03 +0100

    Commit build

--- build/build.js
+++ build/build.js
@@ -1,23 +1,61 @@
 var gui = new dat.GUI();
 var params = {
-    Nombre: 100,
+    Number: 100,
     Random_Seed: 0,
-    P: 0.7,
+    Rotation: 0,
+    Plain_Gradient: 0,
+    Red1: 255,
+    Green1: 255,
+    Blue1: 255,
+    Transparency1: 0,
+    Red2: 255,
+    Green2: 255,
+    Blue2: 255,
+    Transparency2: 0,
     Download_Image: function () { return save(); },
 };
-gui.add(params, "Nombre", 0, 200, 1);
+gui.add(params, "Number", 0, 200, 1);
 gui.add(params, "Random_Seed", 0, 100, 1);
-gui.add(params, "P", 0, 1, 0.001);
+gui.add(params, "Rotation", 0, 180, 1);
+gui.add(params, "Plain_Gradient", 0, 1, 1);
+gui.add(params, "Red1", 0, 255, 1);
+gui.add(params, "Green1", 0, 255, 1);
+gui.add(params, "Blue1", 0, 255, 1);
+gui.add(params, "Transparency1", 0, 255, 1);
+gui.add(params, "Red2", 0, 255, 1);
+gui.add(params, "Green2", 0, 255, 1);
+gui.add(params, "Blue2", 0, 255, 1);
+gui.add(params, "Transparency2", 0, 255, 1);
 gui.add(params, "Download_Image");
 function draw() {
     randomSeed(params.Random_Seed);
     rectMode(CENTER);
     background("white");
     translate(width / 2, height / 2);
-    for (var angle = 0; angle < TWO_PI; angle += TWO_PI / params.Nombre) {
-        var p = p5.Vector.fromAngle(angle).mult(random(5, 250));
+    if (params.Plain_Gradient == 0) {
+        fill(params.Red1, params.Green1, params.Blue1, params.Transparency1);
+    }
+    var t = 0;
+    rotate(params.Rotation);
+    for (var angle = 0; angle < TWO_PI; angle += TWO_PI / params.Number) {
+        var k = random(10, 240);
+        if (params.Plain_Gradient == 1) {
+            var color1 = color(params.Red1, params.Green1, params.Blue1, params.Transparency1);
+            var color2 = color(params.Red2, params.Green2, params.Blue2, params.Transparency2);
+            fill(lerpColor(color1, color2, t));
+            t += 1 / params.Number;
+        }
+        push();
         rotate(angle);
-        rect(p.x, p.y, random(5, 20), random(10, 75));
+        if (k < 40) {
+            rect(k, 0, random(1, 20), random(1, 3));
+        }
+        else if (k < 100) {
+            rect(k, 0, random(7, 30), random(3, 10));
+        }
+        else
+            rect(k, 0, random(15, 70), random(5, 20));
+        pop();
     }
 }
 function setup() {
